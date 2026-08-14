2c2286dfc95cdec0ac3cf0a2ea08cbe323a6bd72
Amit Kumar <46284342+amit-kumaar@users.noreply.github.com>
2026-08-14 12:04:45 +0530

    Add plants route with pagination to Flask app
    Refactored Flask app to include a new route for fetching plants with pagination.

--- __init__.py
+++ __init__.py
@@ -1,22 +1,24 @@
-import importlib
-
-flask = importlib.import_module('flask')
-Flask = flask.Flask
-jsonify = flask.jsonify
+from flask import Flask, jsonify, request
+from models import setup_db, Plant
 
 def create_app(test_config=None):
     app = Flask(__name__)
+    setup_db(app)
 
-    @app.route('/hello')
-    def hello():
-        return jsonify({'message':'Helloooo Amit'})
+    @app.route('/plants', methods=['GET','POST'])
+    #@cross_origin
+    def get_plants():
+        # Implement pagination
+        page = request.args.get('page', 1, type=int)
+        start = (page - 1) * 10
+        end = start + 10
 
-    @app.route('/happy')
-    def happy():
-        return '😊'
+        plants = Plant.query.all()
+        formatted_plants = [plant.format() for plant in plants]
+        return jsonify({
+            'success': True,
+            'plants':formatted_plants[start:end],
+            'total_plants':len(formatted_plants)
+            })
 
-    @app.route('/smiley')
-    def smiley():
-        return ':)'
-
-    return app 
+    return app
